72fbb437dd7fd53186fbff154134416db9d5a7d7
gmvrpw <dev@gmvr.pw>
2022-11-24 17:23:19 +0300

    Добавляет навигацию в приложение

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import { ThemeProvider } from "styled-components";
 import GlobalStyle, { theme } from "./styles";
 import Index from "./pages/Index";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 const App = () => {
   return (
     <ThemeProvider theme={theme.light}>
-      <Index />
+      <Router>
+        <Routes>
+          <Route path={"/index"}>
+            <Index />
+          </Route>
+        </Routes>
+      </Router>
       <GlobalStyle />
     </ThemeProvider>
   );
