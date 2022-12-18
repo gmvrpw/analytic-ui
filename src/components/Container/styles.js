958bc0f5ab7979f9306d53d3b06b35c137fd75c0
Alexander Boiarnikov <111020155+gmvrpw@users.noreply.github.com>
2022-12-18 15:17:30 +0300

    Изменяет внешний вид карточки контейнера (#10)

--- src/components/Container/styles.js
+++ src/components/Container/styles.js
@@ -1,4 +1,4 @@
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 import { GoogleMaterialIcon } from "../Icon";
 import { Link } from "react-router-dom";
 
@@ -8,7 +8,7 @@ export const FallIntoIcon = styled(GoogleMaterialIcon).attrs(() => ({
   position: relative;
   font-size: 12px;
   left: 0;
-  transition: left ease-in 100ms;
+  transition: left linear 100ms;
 `;
 
 export const Container = styled.div`
@@ -19,27 +19,59 @@ export const Container = styled.div`
   border-radius: 1.2rem;
   background: white;
   gap: 15px;
-  height: 320px;
-  :hover ${FallIntoIcon} {
-    left: 5px;
-  }
+  min-height: 320px;
+  min-width: 320px;
+  aspect-ratio: 1 / 1;
 `;
 
-export const Name = styled.div`
+export const ID = styled.div`
   font-family: Inter, sans-serif;
   font-weight: 700;
   font-size: 12px;
   text-align: center;
 `;
 
-export const ID = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: center;
+export const NameWrapper = styled.div`
+  position: relative;
+  height: 20%;
+  overflow: hidden;
+`;
+
+const ClippedNameAnimation = keyframes`
+  0% {
+    transform: translate(0, 0);
+  }
+  20% {
+    transform: translate(0, 0);
+  }
+  80% {
+    transform: translate(calc(-100% + 305px), 0);
+  }
+  100% {
+    transform: translate(calc(-100% + 305px), 0);
+  }
+`;
+
+export const Name = styled.div`
+  text-align: center;
+  overflow: hidden;
+  white-space: nowrap;
+  text-overflow: ellipsis;
   font-family: Dela Gothic One, serif;
   font-weight: 400;
   font-size: 30px;
-  height: 20%;
+  height: 100%;
+  min-width: 100%;
+  color: black;
+  cursor: pointer;
+  :hover {
+    position: absolute;
+    animation: ${ClippedNameAnimation} ${(props) => props.children.length / 8}s
+      linear alternate infinite;
+    overflow: hidden;
+    white-space: nowrap;
+    height: 100%;
+  }
 `;
 
 export const FallInto = styled(Link)`
@@ -49,9 +81,13 @@ export const FallInto = styled(Link)`
   font-family: Inter, sans-serif;
   font-size: 10px;
   color: #a0a0a0;
+  padding: 3px 5px;
   text-decoration: none;
   :hover {
     color: #00a3ff;
+  }
+  :hover ${FallIntoIcon} {
+    left: 4px;
   }
 `;
 
