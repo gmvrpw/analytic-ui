35fbb3369ee22fd2c026ebfb0309108f4fb941e6
gmvrpw <dev@gmvr.pw>
2022-12-18 15:15:46 +0300

    Изменяет внешний вид карточки контейнера

--- src/components/Container/index.js
+++ src/components/Container/index.js
@@ -3,19 +3,21 @@ import * as S from "./styles";
 const Container = ({ id, name, triggers }) => {
   return (
     <S.Container>
-      <S.Name>{name}</S.Name>
       <S.ID>{id}</S.ID>
+      <S.NameWrapper>
+        <S.Name>{name}</S.Name>
+      </S.NameWrapper>
       <S.Triggers>
         {triggers.map((trigger, index) => (
           <S.Trigger key={index}>
             {trigger.element}#{trigger.id}.{trigger.className}
           </S.Trigger>
         ))}
+        <S.FallInto to={`${id}`}>
+          {"все 36"}
+          <S.FallIntoIcon />
+        </S.FallInto>
       </S.Triggers>
-      <S.FallInto to={`${id}`}>
-        {"Весь контейнер"}
-        <S.FallIntoIcon />
-      </S.FallInto>
     </S.Container>
   );
 };
