7911487c608ae6e9641b6487f4f42f327fd82f85
gmvrpw <dev@gmvr.pw>
2022-12-19 08:10:22 +0300

    Добавляет компонент модального окна

---
+++ src/components/Modal/styles.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  z-index: 100000;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background: rgba(0, 0, 0, 0.4);
+`;
